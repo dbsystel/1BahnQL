fd822db176e859fee0001472823db417725d3f30
Dennis Post <dennis.post@deutschebahn.com>
2017-05-12 12:40:51 +0200

    Add Product Class

--- routePart.js
+++ routePart.js
@@ -1,34 +1,39 @@
-const Station = require("./station.js")
+const Station = require('./station.js');
+const Product = require('./product.js');
 
 class RoutePart {
-	
-	constructor(routePartPayload) {
-		this.routePartPayload = routePartPayload
-	}
-	
-	get from() {
-		return new Station(this.routePartPayload.from.id)
-	}
-	
-	get to() {
-		return new Station(this.routePartPayload.to.id)
-	}
-	
-	get delay() {
-		return this.routePartPayload.delay
-	}
-	
-	get direction() {
-		return this.routePartPayload.direction
-	}
-	
-	get start() {
-		return this.routePartPayload.start
-	}
-	
-	get end() {
-		return this.routePartPayload.end
-	}
+
+  constructor(routePartPayload) {
+    this.routePartPayload = routePartPayload;
+  }
+
+  get from() {
+    return new Station(this.routePartPayload.from.id);
+  }
+
+  get to() {
+    return new Station(this.routePartPayload.to.id);
+  }
+
+  get delay() {
+    return this.routePartPayload.delay;
+  }
+
+  get direction() {
+    return this.routePartPayload.direction;
+  }
+
+  get start() {
+    return this.routePartPayload.start;
+  }
+
+  get end() {
+    return this.routePartPayload.end;
+  }
+
+  get product() {
+    return new Product(this.routePartPayload.product);
+  }
 }
 
-module.exports = RoutePart
+module.exports = RoutePart;
