9d3c330ef40385d2c898a2a51e4188bbd6fead4b
Dennis Post <dennis.post@deutschebahn.com>
2017-07-07 15:47:36 +0200

    Add sample test

---
+++ test/sample.test.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('sample testing', () => {
+  it('should be true', () => {
+    expect(true).to.equal(true);
+  });
+});
