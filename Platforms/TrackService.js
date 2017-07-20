e74dc0e97fa391a923c1c8e705f95a002a9f0065
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-20 11:46:35 +0200

    fixes missing lazy resolving

--- Platforms/TrackService.js
+++ Platforms/TrackService.js
@@ -7,32 +7,29 @@ require.extensions['.csv'] = function (module, filename) {
 const tracksFile = require("./DBSuS-Bahnsteigdaten-Stand2017-04.csv");
 
 class TrackService {
-	
-	constructor() {
-		console.log("construct it")
-		this.tracks = new Promise(function(resolve) {
-			parse(tracksFile, {comment: '#', delimiter: ";", columns: true}, function(err, result) {
-				let map = {}
-				result.forEach(function(track) {
-					let stationNumber = track["stationNumber"]
-					let tracks = map[stationNumber] || []
-					tracks.push(new Track(track))
-					map[stationNumber] = tracks
-				})
-				resolve(map)
-			});
-		})
-	}
-	
+
+  get tracks() {
+      if (!this.tracksPromise) {
+        this.tracksPromise = new Promise(function(resolve) {
+    			parse(tracksFile, { comment: '#', delimiter: ";", columns: true}, function(err, result) {
+    				let map = {}
+    				result.forEach(function(track) {
+    					let stationNumber = track["stationNumber"]
+    					let tracks = map[stationNumber] || []
+    					tracks.push(new Track(track))
+    					map[stationNumber] = tracks
+    				})
+    				resolve(map)
+    			});
+    		})
+      }
+      return this.tracksPromise
+  }
+
 	tracksForStationNumber(stationNumber) {
-		return this.tracks.then(tracks => tracks[stationNumber])
-/*
-		return this.tracks.then(tracks => tracks.filter(track => track["stationNumber"] == stationNumber))
-		.then(tracks => tracks.map(track => new Track(track)))
-*/
+		return this.tracks.then(tracks => tracks[stationNumber] || []);
 	}
-	
-	tracksForStationNumberWithTrackName(trackName) {}
+
 }
 
 module.exports = TrackService
