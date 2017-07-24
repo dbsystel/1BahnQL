f0044c7a50dcc10055029f969ddf25df9d22eaca
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-24 10:26:11 +0200

    Adds pictures & photographer into scheme

--- Station/StationRelationships.js
+++ Station/StationRelationships.js
@@ -4,11 +4,12 @@ class StationRelationships {
 	* A StationRelationships connects different datasources related to a station.
 	* @constructor
 	*/
-  constructor(parkingSpaceService, facilityService, timeTableService, trackService) {
+  constructor(parkingSpaceService, facilityService, timeTableService, trackService, stationPictureService) {
     this.parkingSpaceService = parkingSpaceService
     this.facilityService = facilityService
     this.timeTableService = timeTableService
     this.trackService = trackService;
+    this.stationPictureService = stationPictureService
   }
 
   resolve(station) {
@@ -16,6 +17,7 @@ class StationRelationships {
     const facilityService = this.facilityService;
     const timeTableService = this.timeTableService;
     const trackService = this.trackService;
+    const stationPictureService = this.stationPictureService;
 
     station.parkingSpaces = function () {
       return parkingSpaceService.parkingspacesForStationNumber(station.stationNumber);
@@ -24,15 +26,19 @@ class StationRelationships {
     station.facilities = function () {
       return facilityService.facilitiesForStationNumber(station.stationNumber);
     };
-    
+
     station.timetable = function () {
       return timeTableService.timetableForEvaId(station.primaryEvaId);
     };
-    
+
     station.tracks = function() {
-  	  return trackService.tracksForStationNumber(station.stationNumber)
+  	  return trackService.tracksForStationNumber(station.stationNumber);
     }
-    
+
+    station.picture = () => {
+      return stationPictureService.stationPictureForStationNumber(station.stationNumber);
+    }
+
   }
 }
 
