3a5fe53cffc3a7cab6ccfe53355732a18cad2e5a
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-27 11:55:41 +0200

    fixes tests

--- test/Station/StationLoaderMock.js
+++ test/Station/StationLoaderMock.js
@@ -8,6 +8,16 @@ class StationLoaderMock {
     return Promise.resolve(this.result);
   }
 
+  stationByEvaId(evaId) {
+    this.evaId = evaId
+    return Promise.resolve(this.result);
+  }
+
+  stationByRil100(rill100) {
+    this.rill100 = rill100
+    return Promise.resolve(this.result);
+  }
+
   searchStations(searchTerm) {
     this.searchTerm = searchTerm
     return Promise.resolve(this.result);
