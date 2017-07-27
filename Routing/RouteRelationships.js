908f77d35073d9d64abe4d11c1b0f1a8e5b9939f
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-07-27 14:54:57 +0200

    fix indents

--- Routing/RouteRelationships.js
+++ Routing/RouteRelationships.js
@@ -9,25 +9,25 @@ class RouteRelationships {
     const trackService = this.trackService;
 
     route.from = () => {
-			return stationService.stationByEvaId(route.parts[0].fromEvaId);
+      return stationService.stationByEvaId(route.parts[0].fromEvaId);
     }
 
 		route.to = () => {
-			return stationService.stationByEvaId(route.parts[route.parts.length - 1].toEvaId);
+      return stationService.stationByEvaId(route.parts[route.parts.length - 1].toEvaId);
     }
 
     route.parts = route.parts.map((part) => {
       part.from = () => {
-				return stationService.stationByEvaId(part.fromEvaId);
+        return stationService.stationByEvaId(part.fromEvaId);
 			}
       part.to = () => {
-				return stationService.stationByEvaId(part.toEvaId);
+        return stationService.stationByEvaId(part.toEvaId);
 			}
       part.departingTrack = () => {
         return trackService.trackAtStationEvaIdWithTrackNumberNumber(part.fromEvaId, part.departingPlatformNumber);
       }
       part.arrivingTrack = () => {
-				return trackService.trackByEvaIdAndTrackNumber(part.toEvaId, part.arrivingPlatformNumber);
+        return trackService.trackByEvaIdAndTrackNumber(part.toEvaId, part.arrivingPlatformNumber);
       }
 
 			return part
