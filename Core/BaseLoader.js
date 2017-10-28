c63cc693b7854745c2abdd8eea55dd695c669821
Dennis Post <dennis.post@deutschebahn.com>
2017-10-28 23:46:12 +0200

    Add status code check for error handling

--- Core/BaseLoader.js
+++ Core/BaseLoader.js
@@ -1,45 +1,61 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 class BaseLoader {
-
   constructor(APIToken, baseURL) {
     this.APIToken = APIToken;
-    this.fetch = fetch
-    this.baseURL = baseURL
+    this.fetch = fetch;
+    this.baseURL = baseURL;
   }
 
   get fetchConfiguration() {
     const headers = {
-      Authorization: `Bearer ${this.APIToken}`,
+      Authorization: `Bearer ${this.APIToken}`
     };
     const configuration = {
-      method: 'GET',
-      headers,
+      method: "GET",
+      headers
     };
 
     return configuration;
   }
 
   static parseJSON(res, APIName) {
-    return res.json()
-      .catch(error => {
-      let errorMessage;
-      switch (error.type) {
-
-        case 'system': {
-          errorMessage = `${APIName}: Failed to load data`;
+    const resStatusCode = res.status;
+    console.log(`STATUS CODE WAS: ${resStatusCode}`);
+    if (between(resStatusCode, 200, 399)) {
+      return res.json().catch(error => {
+        let errorMessage;
+        switch (error.type) {
+          case "system": {
+            errorMessage = `${APIName}: Failed to load data`;
+            break;
+          }
+          case "invalid-json": {
+            errorMessage = `${APIName}: Failed to parse JSON`;
+            break;
+          }
+          default: {
+            errorMessage = `${APIName}: Unknown Error`;
+            break;
+          }
         }
-        case 'invalid-json': {
-          errorMessage = `${APIName}: Failed to parse JSON`;
-        }
-        default: {
-          errorMessage = `${APIName}: Unknown Error`;
-        }
-        console.warn(errorMessage, res);
+        console.error(errorMessage);
         throw new Error(errorMessage);
-      }
-    })
+      });
+    } else if (between(resStatusCode, 400, 499)) {
+      const errorMessage = `${res.status}: ${res.statusText} while requesting ${res.url}`;
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    } else {
+      const errorMessage = `${res.status}: ${res.statusText} while requesting ${res.url}`;
+      console.error(errorMessage);
+      throw new Error(errorMessage);
+    }
   }
 }
 
-module.exports = BaseLoader
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+
+module.exports = BaseLoader;
