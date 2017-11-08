141b8462293f1b3173c77d5f1448a1f658ec9bd8
Lukas Schmidt <lukas.la.schmidt@deutschebahn.com>
2017-11-08 11:34:59 +0100

    fixes operation location crash

--- OperationLocation/OperationLocationLoader.js
+++ OperationLocation/OperationLocationLoader.js
@@ -3,21 +3,27 @@ const BaseLoader = require('./../Core/BaseLoader');
 const serviceURL = '/betriebsstellen/v1';
 
 class OperationLocationLoader extends BaseLoader {
-
   /**
 	 * Search for bestriebsstellen with a given searchterm.
 	 * @param {string} name - A term you want to search for.
 	 * @return {Promise<Array<BetriebsstellenJSON>>}
 	 */
-	search(name) {
-		const url = `${this.baseURL}${serviceURL}/betriebsstellen?name=${name}`;
-		const configuration = this.fetchConfiguration;
-		const promies = this.fetch(url, configuration)
-		.then(res => OperationLocationLoader.parseJSON(res, "betriebsstellen"))
-		.then(result => result || [])
+  search(name) {
+    const url = `${this.baseURL}${serviceURL}/betriebsstellen?name=${name}`;
+    const configuration = this.fetchConfiguration;
+    const promies = this.fetch(url, configuration)
+	  .then(res => OperationLocationLoader.parseJSON(res, 'betriebsstellen'))
+	  .then(function(result) { 
+		  if((!!result) && (result.constructor === Array)) {
+			  return result
+		  } else {
+			  return []
+		  }
+	   })
+      .then(result => result || []);
 
-		return promies;
-	}
+    return promies;
+  }
 }
 
 module.exports = OperationLocationLoader;
